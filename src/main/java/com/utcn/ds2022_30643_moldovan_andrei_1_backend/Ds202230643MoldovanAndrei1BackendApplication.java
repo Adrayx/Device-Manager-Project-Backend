8e6f98ca53166d4f6b6e2ecea6e9e653477c4e16
Andrei Moldovan <65128250+Adrayx@users.noreply.github.com>
2022-10-13 09:12:19 +0300

    initial commit

---
+++ src/main/java/com/utcn/ds2022_30643_moldovan_andrei_1_backend/Ds202230643MoldovanAndrei1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.utcn.ds2022_30643_moldovan_andrei_1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds202230643MoldovanAndrei1BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ds202230643MoldovanAndrei1BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utcn/ds2022_30643_moldovan_andrei_1_backend/Ds202230643MoldovanAndrei1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utcn.ds2022_30643_moldovan_andrei_1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds202230643MoldovanAndrei1BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
